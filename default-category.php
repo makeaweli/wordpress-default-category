84643afec72b58143ecac9c5a9f5bb54c8fa9606
Jason Kalawe <jason@kalawe.com>
2014-03-04 13:24:43 -1000

    Everything works

--- default-category.php
+++ default-category.php
@@ -4,21 +4,29 @@
  * @version 1.0
  */
 /*
-Plugin Name: SOS Category
+Plugin Name: Default Category
 Plugin URI: http://wordpress.org/plugins/default-text/
-Description: Auto select category based on Gemini site
+Description: Auto select category.
 Author: Jason M. Kalawe
 Version: 1.0
 Author URI: http://makea.kalawe.com
 
 */
 
+include_once dirname( __FILE__ ) . '/settings.php';
 
 /*
- * Save the default category before it's rendered
+ * Save the default category before it's rendered to the user
  */
 function default_category_save($post_ID) {
-  wp_set_post_categories( $post_ID, array('3') );
+  
+  $post_categories = wp_get_post_categories( $post_ID);
+  if(empty($post_categories)) {
+    if(get_option('default_category_id')) {
+      $default_category_id = get_option('default_category_id'); 
+      wp_set_post_categories( $post_ID, $default_category_id['default_category_id'] );  
+    }
+  }
 
 }
 add_action( 'save_post', 'default_category_save' );
